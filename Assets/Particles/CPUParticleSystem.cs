543e2c24344799faf9289e436cc2cde412c659e5
Nico Auerswald <auersw.nico@gmail.com>
2026-04-17 10:19:27 +0200

    spawning and debug drawing particles

---
+++ Assets/Particles/CPUParticleSystem.cs
@@ -0,0 +1,109 @@
+using UnityEngine;
+
+using Random = UnityEngine.Random;
+
+public struct Particle2D
+{
+    public Vector2 position;
+    public Vector2 previousPosition;
+
+    public Particle2D(Vector2 position, Vector2 velocity)
+    {
+        this.position = position;
+        this.previousPosition = position - velocity;
+    }
+}
+
+public class CPUParticleSystem : MonoBehaviour
+{
+    [SerializeField, Range(0, 100000)] int _particleCount;
+    [SerializeField, Range(0.0f, 2.0f)] float _particleDrawRadius = 0.5f;
+    [SerializeField] Rect _boundingRect;
+
+    [Header("Debug Flags")]
+    [SerializeField] bool _drawParticlePreviousPosition = true;
+
+    [Header("Colors")]
+    [SerializeField] Color _boundingRectColor = Color.grey;
+    [SerializeField] Color _particleColor = Color.white;
+    [SerializeField] Color _previousPositionColor = Color.white;
+
+
+    Particle2D[] particles;
+
+    protected void Start()
+    {
+        SpawnParticles(_boundingRect, _particleCount);
+    }
+
+    protected void Update()
+    {
+        
+    }
+
+    protected void FixedUpdate()
+    {
+        
+    }
+
+    protected void OnDrawGizmos()
+    {
+        DrawBoundingRect();
+        DrawParticles();
+    }
+
+
+    void SpawnParticles(Rect boundingRect, int count)
+    {
+        particles = new Particle2D[count];
+
+        for (int i = 0; i < count; i++)
+        {
+            float posX = Random.Range(boundingRect.xMin, boundingRect.xMax);
+            float posY = Random.Range(boundingRect.yMin, boundingRect.yMax);
+            Vector2 pos = new Vector2(posX, posY);
+
+            float dirX = Random.Range(-1.0f, 1.0f);
+            float dirY = Random.Range(-1.0f, 1.0f);
+            Vector2 dir = new Vector2(dirX, dirY);
+
+            float mag = Random.Range(0.1f, 1.0f);
+
+            Vector2 vel = dir.normalized * mag;
+
+            particles[i] = new Particle2D(pos, vel);
+        }
+    }
+
+    void DrawBoundingRect()
+    {
+        DebugUtils.DrawRect(_boundingRect, _boundingRectColor);
+    }
+
+    void DrawParticles()
+    {
+        if (particles != null)
+        {
+
+            for (int i = 0; i < particles.Length; i++)
+            {
+                Gizmos.color = _particleColor;
+
+                Particle2D p = particles[i];
+
+                Vector2 position = p.position;
+                Vector2 previousPosition = p.previousPosition;
+
+                Gizmos.DrawSphere(position, _particleDrawRadius);
+
+                if (_drawParticlePreviousPosition)
+                {
+                    Gizmos.color = _previousPositionColor;
+                    Gizmos.DrawSphere(previousPosition, _particleDrawRadius * 0.5f);
+                    Gizmos.color = Color.darkGray;
+                    Gizmos.DrawLine(position, previousPosition);
+                }
+            }
+        }
+    }
+}
